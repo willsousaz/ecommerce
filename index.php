8ce0d219c8aedeec080ab39b076104834af2fee4
Yuri Wilson <yuri.wilson18@gmail.com>
2020-09-12 17:00:13 -0300

    Criando PageAdmim e tplAdmin

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 
 	use \Slim\Slim;
 	use \Ytech\Page;
+	use \Ytech\PageAdmin;
 
 	$app = new Slim();
 
@@ -12,6 +13,14 @@
 	$app->get('/',function(){
 
 		$page = new Page();
+
+		$page->setTpl("index");
+
+	});
+
+		$app->get('/admin',function(){
+
+		$page = new PageAdmin();
 
 		$page->setTpl("index");
 
---
+++ vendor/ytech/php-classes/src/PageAdmin.php
@@ -0,0 +1,17 @@
+<?php 
+
+	namespace Ytech;
+
+
+	/**
+	 * 
+	 */
+	class PageAdmin extends Page
+	{
+		
+		function __construct($opts = array(), $tpl_dir = "/views/admin/")
+		{
+			parent::__construct($opts,$tpl_dir);
+		}
+	}
+ ?>
