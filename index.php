0bca85e2d93285de371792cd7a5faa252c6060bc
unknown <yuri.wilson18@gmail.com>
2020-11-13 21:51:04 -0300

    Criação de Categorias

--- index.php
+++ index.php
@@ -356,6 +356,21 @@
 
 	});
 
+	$app->get("/categories/:idcategory",function($idcategory){
+
+		$category = new Category();
+
+		$category->get((int)$idcategory);
+
+		$page = new Page();
+
+		$page->setTpl("category",[
+			'category'=>$category->getValues(),
+			'products'=>[]
+		]);
+
+	});
+
 	$app->run();
 
  ?>
--- vendor/ytech/php-classes/src/Model/Category.php
+++ vendor/ytech/php-classes/src/Model/Category.php
@@ -33,6 +33,8 @@
 
 		$this->setData($results[0]);
 
+		Category::updateFile();
+
 	}
 
 	
@@ -74,6 +76,22 @@
 		$sql->query("DELETE FROM tb_categories WHERE idcategory = :idcategory",[
 			':idcategory'=>$this->getidcategory()
 		]);
+
+		Category::updateFile();
+	}
+
+	public static function updateFile(){
+
+		$categories = Category::listAll();
+
+		$html = [];
+
+		foreach ($categories as $row) {
+			array_push($html,   '<li><a href="/categories/'.$row['idcategory'] .'">'.$row['descategory'].'</a></li>');
+		}
+
+		file_put_contents($_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR."views". DIRECTORY_SEPARATOR."categories-menu.html",implode('',$html));
+
 	}
 
 	
