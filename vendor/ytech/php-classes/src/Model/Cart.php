feaa02fca74673d5e93750cdb0a57dee671927bf
unknown <yuri.wilson18@gmail.com>
2020-12-10 21:28:19 -0300

    Carrinho de compras Completo

--- vendor/ytech/php-classes/src/Model/Cart.php
+++ vendor/ytech/php-classes/src/Model/Cart.php
@@ -85,7 +85,9 @@
 				':idcart'=>$idcart
 			]);
 
-			if (count($results > 0)) {
+			$count = count($results);
+
+			if ($count > 0) {
 				$this->setData($results[0]);
 			}
 
@@ -106,6 +108,57 @@
 			]);
 
 			$this->setData($results[0]);
+
+		}
+
+		public function addProduct(Product $product){
+
+			$sql = new Sql();
+
+
+			$sql->query("INSERT INTO tb_cartsproducts (idcart, idproduct) VALUES(:idcart,:idproduct)",[
+				':idcart'=>$this->getidcart(),
+				'idproduct'=>$product->getidproduct()
+			]);
+
+		}
+
+
+		public function removeProduct(Product $product , $all = false){
+
+		$sql = new Sql();
+
+			if($all ){
+
+				$sql->query("UPDATE tb_cartsproducts SET dtremoved = NOW() WHERE idcart =:idcart AND idproduct = :idproduct  AND  dtremoved IS NULL ",[
+					':idcart'=>$this->getidcart(),
+					'idproduct'=>$product->getidproduct()	
+				]);
+
+				}else{
+
+					$sql->query("UPDATE tb_cartsproducts SET dtremoved = NOW() WHERE idcart =:idcart AND idproduct = :idproduct AND  dtremoved IS NULL LIMIT 1",[
+					':idcart'=>$this->getidcart(),
+					'idproduct'=>$product->getidproduct()	
+				]);
+				}
+		}
+
+		public function getProduct(){
+
+			$sql =  new Sql();
+
+			$rows =  $sql->select("
+			SELECT b.idproduct, b.desproduct,b.vlprice,b.vlwidth,b.vlheight,b.vllength,b.vlweight , b.desurl ,COUNT(*) AS nrqtd, SUM(b.vlprice) AS vltotal
+			FROM tb_cartsproducts a INNER JOIN tb_products b ON a.idproduct = b.idproduct 
+			WHERE a.idcart = :idcart AND a.dtremoved IS NULL
+			GROUP BY b.idproduct, b.desproduct,b.vlprice,b.vlwidth,b.vlheight,b.vllength,b.vlweight, b.desurl
+			ORDER BY b.desproduct",
+			[
+				':idcart'=>$this->getidcart()
+			]);
+
+			return Product::checkList($rows);
 
 		}
 
